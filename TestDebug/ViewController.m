ba23d631ed9affe8bda0c7ec9a6da7aad9a1931c
idu <375662923@qq.com>
2017-05-27 16:43:44 +0800

    苹果原生界面调试工具
    苹果原生界面调试工具

--- TestDebug/ViewController.m
+++ TestDebug/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UIViewController+Debugging.h"
 @interface ViewController ()
 
 @end
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  [self showDebugger];
 }
 
 
